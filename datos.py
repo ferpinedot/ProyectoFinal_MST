ae87af8d4fc569eff47ec7ed00d55d58c72fa7c3
ferpinedot <47230287+ferpinedot@users.noreply.github.com>
2020-04-27 15:42:39 -0500

    Add files via upload

---
+++ datos.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 27 15:25:08 2020
+
+@author: User
+"""
+
+# -- ------------------------------------------------------------------------------------ -- #
+# -- proyecto: Microestructura y Sistemas de Trading - Laboratorio 4 - Proyecto Final
+# -- archivo: datos.py - para jalar los datos
+# -- mantiene: Fernanda Pinedo, Oscar Flores, Francisco Rodriguez
+# -- repositorio: https://github.com/ferpinedot/proyecto_equipo5
+# -- ------------------------------------------------------------------------------------ -- #
---
+++ visualizaciones.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 27 15:29:33 2020
+
+@author: User
+"""
+
+# -- ------------------------------------------------------------------------------------ -- #
+# -- proyecto: Microestructura y Sistemas de Trading - Laboratorio 4 - Proyecto Final
+# -- archivo: visualizaciones.py 
+# -- mantiene: Fernanda Pinedo, Oscar Flores, Francisco Rodriguez
+# -- repositorio: https://github.com/ferpinedot/proyecto_equipo5
+# -- ------------------------------------------------------------------------------------ -- #
