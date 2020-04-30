79da02ab96666ecca959fe455dc3944cf502d6d8
ferpinedot <mfernandapinedot@hotmail.com>
2020-04-30 07:44:35 -0500

    funciones estadisticas

--- funciones.py
+++ funciones.py
@@ -9,7 +9,7 @@ Created on Mon Apr 27 15:28:14 2020
 # -- proyecto: Microestructura y Sistemas de Trading - Laboratorio 4 - Proyecto Final
 # -- archivo: funciones.py - donde se llevarán a cabo las funciones a utilizar
 # -- mantiene: Fernanda Pinedo, Oscar Flores, Francisco Rodriguez
-# -- repositorio: https://github.com/ferpinedot/proyecto_equipo5
+# -- repositorio: https://github.com/OscarFlores-IFi/proyecto_equipo5
 # -- ------------------------------------------------------------------------------------ -- #
 
 
@@ -19,8 +19,13 @@ import pandas_datareader.data as web
 import datos as dt
 from oandapyV20 import API
 import oandapyV20.endpoints.instruments as instruments
-from matplotlib import pyplot
-from statsmodels.graphics.tsaplots import plot_acf
+import statsmodels.api as sm
+from statsmodels.tsa.stattools import adfuller
+import statsmodels.stats.diagnostic as smd
+from statsmodels.formula.api import ols
+import scipy.stats
+from scipy.stats import shapiro
+from scipy.stats import normaltest
 
 
 #%% Parte 2: Datos Históricos
@@ -48,6 +53,122 @@ def f_leer_archivo(param_archivo, sheet_name = 0):
     return df_data
 
 
+# Autocorrelación
+def f_autocorr(datos):
+    """
+    Parameters
+    ----------
+    series : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    None.
+
+    """
+    # Seleccionar la serie como el valor actual del indicador
+    serie = datos['actual']
+    autocorr = sm.stats.diagnostic.acorr_ljungbox(serie, lags=None, boxpierce=False)
+    lbva = pd.DataFrame(autocorr[0])
+    lbva.columns = ['Valor']
+    pva = pd.DataFrame(autocorr[1])
+    pva.columns = ['Valor']
+    pva = pva.round(5)
+
+    return {'Valor Test Estadístico' : lbva.copy(), 'P-value' : pva.copy()}
     
 
+# Autocorrelación parcial
+def f_autocorr_par(datos):
+    """
+    Parameters
+    ----------
+    datos : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    None.
+    """
+    serie = datos['actual']
+    autocorr_par = sm.tsa.stattools.pacf(serie, method = 'yw', alpha = 0.01)
+    facp = pd.DataFrame(autocorr_par[0])
+    facp.columns = ['Valor']
+    facp = facp.round(2)
+    int_conf = pd.DataFrame(autocorr_par[1])
+    int_conf.columns = ['Límite inferior', 'Límite superior']
+    return {'Autocorrelaciones Parciales': facp.copy(), 'Intervalos de confianza': int_conf.copy()}
+
+
+# Prueba de Normalidad
+def f_norm_shw(datos):
+    """
+    Parameters
+    ----------
+    datos : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    None.
+
+    """
+    serie = datos['actual']
+    norm_shap_w = shapiro(serie)
+    stat_shw = norm_shap_w[0]
+    pvalue_shw = norm_shap_w[1]
+    alpha = 0.05
+    normal_shw = 'Si' if pvalue_shw > alpha else 'No'
+    return {'Statistic Value': stat_shw, 'P-value': pvalue_shw, '¿Normal?': normal_shw}
+    
+
+def f_norm_dagp(datos):
+    """
+    Parameters
+    ----------
+    datos : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    None.
+
+    """
+    serie = datos['actual']    
+    norm_dagp = normaltest(serie)
+    stat_dagp = norm_dagp[0]
+    pvalue_dagp = norm_dagp[1]
+    alpha = 0.05
+    normal_dagp = 'Si' if pvalue_dagp > alpha else 'No'
+    return {'Statistic Value': stat_dagp, 'P-value': pvalue_dagp, '¿Normal?': normal_dagp}
+
+
+# Estacionariedad
+def f_stationarity(datos):
+    """
+    Parameters
+    ----------
+    datos : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    None.
+
+    """
+    serie = datos['actual']
+    # resultado = adfuller(serie)
+    # return {'Dicky Fuller Test Statistic': resultado[0], 'P-Value': resultado[1], 'Valores críticos': resultado[4]}
+    
+    stc = sm.tsa.stattools.adfuller(serie, maxlag = 2, regression = "ct", autolag = 'AIC', store = False, regresults = False)
+    adf = stc[0]
+    pv = stc[1]
+    ul = stc[2]
+    nob = stc[3]
+    cval = stc[4]
+    icmax = stc[5] 
+    alpha = 0.05
+    stty = 'No' if pv > alpha else 'Si'
+    return {'Dicky Fuller Test Statistic': adf, 'P-Value': pv, 'Número de rezagos': ul, 'Número de observaciones': nob, 'Valores críticos': cval, 'Criterior de información maximizada': icmax, '¿Estacionaria?': stty}
+   
 
