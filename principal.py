19a8e5c16fe56c36356267dd9fa4b1ebd3026e92
OscarFlores-IFi <if715029@gmail.com>
2020-04-30 10:30:00 -0500

    descarga precios

--- principal.py
+++ principal.py
@@ -14,10 +14,16 @@ Created on Mon Apr 27 15:28:55 2020
 
 import funciones as fn
 import visualizaciones as vn
+import pandas as pd
+
 
 # Leer el archivo: Indicador económico USA
 datos = fn.f_leer_archivo(param_archivo='archivos/FedInterestRateDecision-UnitedStates.xlsx', sheet_name= 0)
 vn.v_indicador_orig(datos)
+
+oatk='107596e9d65c' + '1bbc9175953d917140' + '12-f975c6201dddad03ac1592232c0ea0ea'
+print(fn.f_precios_masivos(datos.datetime[0], datos.datetime[0] + pd.to_timedelta('00:30:00'),
+                        'M1', "EUR_USD", oatk,  p5_ginc=4900))
 
 # Autocorrelación del Indicador
 fac = fn.f_autocorr(datos)
