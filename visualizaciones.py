2f55d6b1611915cdb3d44f3f9d07f12c0f4eeaef
ferpinedot <mfernandapinedot@hotmail.com>
2020-05-07 19:41:13 -0500

    stats graphs

--- visualizaciones.py
+++ visualizaciones.py
@@ -13,23 +13,27 @@ Created on Mon Apr 27 15:29:33 2020
 # -- ------------------------------------------------------------------------------------ -- #
 
 
-import funciones as fn
+import funciones_1d as fn
 import matplotlib.pyplot as plt
 from statsmodels.graphics.tsaplots import plot_acf
 from statsmodels.graphics.tsaplots import plot_pacf
 from statsmodels.graphics.gofplots import qqplot
 # from statsmodels.tsa.seasonal import seasonal_decompose
 import pandas as pd
-# import statsmodels.api as sm
+import statsmodels.api as sm
 from statsmodels.tsa.seasonal import STL
 import seaborn as sb
 from statsmodels.tsa.arima_model import ARIMA
 from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
+from scipy import stats
+from scipy.stats import normaltest
+
 
 #%%
 # Datos a usar para gráficas
 datos = fn.f_leer_archivo(param_archivo='archivos/FedInterestRateDecision-UnitedStates.xlsx', sheet_name= 0)
+
 
 # Visualización del indicador origial con datos 'actual'
 def v_indicador_orig(datos):
@@ -95,7 +99,6 @@ def v_indicador_dif(datos):
     datos_dif.dropna(inplace= True)
     serie = datos_dif['actual']
     plt.plot(serie, color = 'blue')
-    # plt.title('Indicador con una diferencia')
     plt.title('Indicador con una diferencia')
     plt.show()
    
@@ -277,8 +280,39 @@ def v_norm_qq(datos):
     plt.xlabel('Cuantiles teóricos')
     plt.ylabel('Cuantiles de muestra')
     plt.show()
+     
     
+# Visualización de normalidad con residuales   
+def v_norm_resids(datos):
+    """
+    Visualización de la prueba de normalidad con residuales del modelo ARIMA
     
+    Parameters
+    ----------
+    datos : pd.DataFrame : con información contenida en archivo leido
+
+    Returns
+    -------
+    Gráfica tipo histograma de los residuales del modelo ARIMA
+
+    """
+    datos = fn.f_leer_archivo(param_archivo='archivos/FedInterestRateDecision-UnitedStates.xlsx', sheet_name= 0)
+    datos = datos.set_index('datetime')
+    datos_dif = datos - datos.shift()
+    datos_dif.dropna(inplace= True)
+    datos_dif_ac = datos_dif['actual']
+    model = sm.tsa.ARIMA(datos_dif_ac, order = (3,1,3)).fit(disp=False)
+    resid = model.resid
+    fig = plt.figure(figsize = (12,8))
+    ax0 = fig.add_subplot(111)
+    sb.distplot(resid, fit = stats.norm, ax = ax0)
+    (mu, sigma) = stats.norm.fit(resid)
+    plt.legend(['Normal dist. ($\mu=$ {:.2f} y $\sigma=$ {:.2f} )'.format(mu, sigma)], loc='best')
+    plt.ylabel('Frecuencia')
+    plt.title('Distribución de residuales')
+    fig = plt.figure(figsize=(12,8))
+            
+
 # Detección de atípicos boxplot 
 def v_det_at(datos):
     """
@@ -301,6 +335,7 @@ def v_det_at(datos):
     plt.show()
 
 
+# Visualización de atípicos de Indicador diferenciado por boxplot
 def v_det_at_dif(datos):
     """
     Visualización de los datos atípicos del indicador estacionario por medio de un boxplot 
@@ -322,6 +357,5 @@ def v_det_at_dif(datos):
     dat_at = sb.boxplot(data = serie, orient = 'v')
     dat_at.set_title('Datos atípicos del Indicador')
     dat_at.set_ylabel('Valor actual')
-    plt.show()
-    
+    plt.show() 
     
